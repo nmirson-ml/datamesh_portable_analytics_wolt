2ff50b1306c12d913a95aa050453c0e98c5177d0
Constantin Lungu <lungupconstantin@gmail.com>
2024-11-08 14:33:19 +0000

    align with parent project

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ from pathlib import Path
 
 from sqlmesh.dbt.loader import sqlmesh_config
 
-config = sqlmesh_config(Path(__file__).parent, variables={"DUCKDB_FILE_PATH":"./shared/db/datamart.duckdb",
-"INPUT_FILES_PATH":"./shared/parquet"})
+config = sqlmesh_config(Path(__file__).parent)
 
